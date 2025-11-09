4f4b35bb417b7dcbbde34cf44323c71f7009cc84
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-09 22:58:14 +0000

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,1 +1,12 @@
+// Task 3 - printTeacher function
+// ===========================
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher(teacher3.firstName, teacher3.lastName)); // J. Doe
+console.log(printTeacher(director1.firstName, director1.lastName)); // J. Doe
