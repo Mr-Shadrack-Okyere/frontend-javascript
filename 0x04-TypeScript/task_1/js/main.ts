356eb74e7af9c7f64f721fbb6ffad7a13d9d459c
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-09 22:18:15 +0000

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,27 +1,53 @@
-// main.ts
+// task_1/js/main.ts
 
-// Define the Teacher interface
+// 1. Teacher interface
 interface Teacher {
-  firstName: string;
-  lastName: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // allows adding any other attribute dynamically
+  [key: string]: any; // allow any extra properties
 }
 
-// Define the Directors interface that extends Teacher
+// 2. Directors interface (extends teacher)
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// Example usage
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+// 3. printTeacher function + interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-console.log(director1);
+// 4. StudentClass + interfaces
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
