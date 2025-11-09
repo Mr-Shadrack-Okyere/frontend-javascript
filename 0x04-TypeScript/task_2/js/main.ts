3f98c669bed36bb96494ca478cd0c30eb0a37135
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-09 22:56:42 +0000

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,1 +1,16 @@
+// ===========================
+// Task 2 - Directors interface (extends Teacher)
+// ===========================
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
