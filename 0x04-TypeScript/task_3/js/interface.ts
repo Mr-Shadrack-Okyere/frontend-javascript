5da129408e0d292d7627ff2003c04b00bab3b921
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-09 23:09:07 +0000

    Update interface.ts

--- 0x04-TypeScript/task_3/js/interface.ts
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -1,1 +1,9 @@
+// Define a type for row IDs
+export type RowID = number;
 
+// Define an interface for row elements
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
